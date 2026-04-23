4da75cc91ee496fd32bc7ad0afdcbd4cf890ecbc
aekinkehara <aekinkehara@gmail.com>
2026-04-23 13:40:00 +0700

    ngubah widget lightbrownbtn jadi svg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:braga8_app_components/views/meter-input/view_meter_screen.dart';
 import 'package:flutter/material.dart';
 import 'views/dashboard/dashboard_screen.dart';
 
@@ -32,7 +31,7 @@ class Braga8App extends StatelessWidget {
         textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.white)),
       ),
       debugShowCheckedModeBanner: false,
-      home: ViewMeterScreen(tenantName: 'solaria', unitData: {},),
+      home: DashboardScreen(),
     );
   }
 }
--- lib/widgets/light_brown_btn.dart
+++ lib/widgets/light_brown_btn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LightBrownBtn extends StatelessWidget {
   final VoidCallback onTap;
@@ -8,73 +9,28 @@ class LightBrownBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 55,
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Color(0xFF6A3013).withValues(alpha: 0.3),
-            blurRadius: 10,
-            offset: Offset(4, 6),
-          ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(18),
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(18),
+      child: AspectRatio(
+        aspectRatio: 335 / 70, 
         child: Stack(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.black.withValues(alpha: 0.2),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF76240B).withValues(alpha: 0.1),
-              ),
-            ),
             Positioned.fill(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 0.5),
-                      blurRadius: 10,
-                      spreadRadius: -5,
-                      offset: Offset(4, 2),
-                    ),
-                  ],
-                ),
+              child: SvgPicture.asset(
+                'assets/loginbtn.svg',
+                fit: BoxFit.fill, 
               ),
             ),
-            Positioned.fill(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  border: Border.all(
-                    color: Colors.white.withValues(alpha: 0.2),
-                    width: 1.0,
-                  ),
-                ),
-              ),
-            ),
-            Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: onTap,
-                borderRadius: BorderRadius.circular(18),
-                child: Center(
-                  child: Text(
-                    label,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 0.5,
-                    ),
-                  ),
+            Align(
+              alignment: const Alignment(0.0, -0.2), 
+              child: Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5,
                 ),
               ),
             ),
